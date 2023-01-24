94ffbcf94ad6c3df968263a8cd4b43942ed4a10c
MonikaTychy <mo.tychy@gmail.com>
2023-01-24 16:54:30 +0100

    implemented all functonality to the game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function App() {
+    const [text, setText] = useState("")
+    const [timer, setTimer] = useState(5)
+    const [isTimeRemaning, setIsTimeRemaning] = useState(false)
+    const [wordsCount, setWordCounts] = useState(0)
+    const textareaRef = useRef(null)
+
+    useEffect(() => {
+      if(timer > 0 && isTimeRemaning){
+        setTimeout(() => {
+          setTimer(prevTimer => prevTimer - 1)
+        }, 1000)
+      } else if (timer === 0) {
+        setIsTimeRemaning(false)
+        setWordCounts(countWords(text))
+      }
+    }, [timer, isTimeRemaning])
+
+    function startGame() {
+       setIsTimeRemaning(true)
+       setTimer(5)
+       setText("")
+       setWordCounts(0)
+       textareaRef.current.disabled = false
+       textareaRef.current.focus()
+    }
+
+   function handleChange(e) {
+       setText(e.target.value)
+   }
+
+   function countWords(str) {
+    return str === "" ? 0 : str.trim().split(" ").length
+    }
+
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time remaining: 0</h4>
-      <button>start</button>
-      <h4>Word count: 0</h4>
+      <textarea 
+         onChange={handleChange}
+         value={text}
+         disabled={!isTimeRemaning}
+         ref={textareaRef}
+      />
+      <h4>Time remaining: {timer}</h4>
+      <button 
+         onClick={startGame}
+         disabled={isTimeRemaning}
+      >
+      {timer === 0 ? "play again" : "start"}</button>
+      {timer === 0 ? <h4>Word count: {wordsCount}</h4> : null}
     </div>
   );
 }
